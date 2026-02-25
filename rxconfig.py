fb4c90da56da40680c91bbd020ac08bca629a502
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-02-26 02:46:41 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,7 @@
-mport reflex as rx
+import reflex as rx
 
 config = rx.Config(
     app_name="Navika",
+    api_url="https://navika-gold-book.reflex.run",
+    deploy_url="https://navika-gold-book.reflex.run",
 )
