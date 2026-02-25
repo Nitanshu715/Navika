67b365174dec24bc3b6a000dc61181fb6ab36082
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-02-25 14:13:12 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="finance_ai",
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
+    app_name="Navika",
 )
