2e539380975083482b6ccb0edff9cc7984931578
Nitanshu715 <nitanshutak070105@gmail.com>
2026-03-19 22:52:07 +0530

    final: set correct URL for silver-piano app

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ from reflex.plugins.sitemap import SitemapPlugin
 
 config = rx.Config(
     app_name="Navika",
-    api_url="https://navika-gold-star.reflex.run",
-    deploy_url="https://navika-gold-star.reflex.run",
+    api_url="https://navika-silver-piano.reflex.run",
+    deploy_url="https://navika-silver-piano.reflex.run",
     disable_plugins=[SitemapPlugin],
 )
