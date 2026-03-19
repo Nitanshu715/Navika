eeb467f92245d28b238a48035c8930b0be06c787
Nitanshu715 <nitanshutak070105@gmail.com>
2026-03-19 21:35:32 +0530

    fix: add numpy to requirements and fix rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,9 @@
 import reflex as rx
+from reflex.plugins.sitemap import SitemapPlugin
 
 config = rx.Config(
     app_name="Navika",
     api_url="https://navika-gold-star.reflex.run",
     deploy_url="https://navika-gold-star.reflex.run",
-    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+    disable_plugins=[SitemapPlugin],
 )
